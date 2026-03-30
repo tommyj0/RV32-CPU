b625efe7b68dc8c5ffbf7ea86603cb2a1d35c638
tommyj0 <thomasfj17@hotmail.com>
2026-03-30 22:33:47 +0100

    swapped instr logic

--- rtl/instr_fetch.v
+++ rtl/instr_fetch.v
@@ -20,7 +20,8 @@ wire    [31:0]  Next_PC;
 reg     [31:0]  PC_reg;
 
 assign PC_if_id = PC_reg;
-assign Next_PC = (branch_mem_if) ? PC_branch_mem_if : (stall) ? PC_reg : PC_reg + 4;
+// assign Next_PC = (branch_mem_if) ? PC_branch_mem_if : (stall) ? PC_reg : PC_reg + 4;
+assign Next_PC = (stall) ? PC_reg : (branch_mem_if) ? PC_branch_mem_if : PC_reg + 4;
 
 always@(posedge clk)
 begin
